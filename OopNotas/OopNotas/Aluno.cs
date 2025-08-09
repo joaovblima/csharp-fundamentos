38e705027c4de504b9efebd1dc65a132b6dcb8e6
João Lima <joaovictorbernardinodelima@gmail.com>
2025-08-09 18:39:26 -0300

    add exercicio

---
+++ OopNotas/OopNotas/Aluno.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace OopNotas
+{
+    class Aluno
+    {
+        public string Nome;
+        public double[] Notas;
+
+        public double CalculaMedia(double[] Notas)
+        {
+            double soma = 0;
+            foreach (var nota in Notas)
+            {
+                soma += nota;
+            }
+            return soma;
+        }
+
+        public bool Aprovado()
+        {
+            if (CalculaMedia(Notas) < 60)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ OopNotas/OopNotas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace OopNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome do aluno: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite três notas do aluno: ");
+            double[] notas = new double[3];
+            notas[0] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            notas[1] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            notas[2] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Aluno aluno = new Aluno();
+            aluno.Nome = nome;
+            aluno.Notas = notas;
+            Console.WriteLine("Media = "+ aluno.CalculaMedia(notas).ToString("F2", CultureInfo.InvariantCulture));
+            if (aluno.Aprovado() == false)
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60.0 - aluno.CalculaMedia(notas)).ToString("F2", CultureInfo.InvariantCulture) + " PONTOS.");
+            }
+
+        }
+    }
+}
