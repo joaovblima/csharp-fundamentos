8bad345a31bfa143bbffe1ee4b764c2dc49b8c1a
João Lima <joaovictorbernardinodelima@gmail.com>
2025-08-13 01:15:40 -0300

    add exercicio

---
+++ Pensionato/Pensionato/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Pensionato
+{
+     class Aluno
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    
+        
+        public Aluno(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Email;
+        }
+
+    }
+}
---
+++ Pensionato/Pensionato/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] aluno = new Aluno[10];
+            Console.Write("How many rooms will be rented? ");
+            int roomNums = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= roomNums; i++)
+            {
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                aluno[room] = new Aluno(name, email);
+
+
+
+            }
+            Console.WriteLine("Busy rooms: ");
+            for (int i = 0; i < aluno.Length; i++)
+            {
+                if (aluno[i] != null)
+                {
+                    Console.WriteLine(i + ": " + aluno[i]);
+                }
+            }
+        }
+    }
+}
